fa14ef77545a26e52a796dc635f1c7a7aa89d0c7
mukund kumar <mukund5457@gmail.com>
2023-12-15 16:34:07 +0530

    added file uploding part

--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import AppError from "../utils/error.util.js";
 import User from '../models/user.model.js'
-
+import cloudinary from 'cloudinary';
+import fs from 'fs/promises';
 const cookieOptions = {
     secure:true,
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
@@ -44,6 +45,34 @@ const register=async(req,res,next)=>{
   }
 
   //TODO:File Upload
+
+  if (req.file) {
+    console.log(req.file);
+    try {
+      const result = await cloudinary.v2.uploader.upload(req.file.path, {
+        folder: 'lms', // Save files in a folder named lms
+        width: 250,
+        height: 250,
+        gravity: 'faces', // This option tells cloudinary to center the image around detected faces (if any) after cropping or resizing the original image
+        crop: 'fill',
+      });
+
+      // If success
+      if (result) {
+        // Set the public_id and secure_url in DB
+        user.avatar.public_id = result.public_id;
+        user.avatar.secure_url = result.secure_url;
+
+        // After successful upload remove the file from local storage
+        fs.rm(`uploads/${req.file.filename}`);
+      }
+    } catch (error) {
+      return next(
+        new AppError(error || 'File not uploaded, please try again', 400)
+      );
+    }
+  }
+
   await user.save();
 
    // Setting the password to undefined so it does not get sent in the response
