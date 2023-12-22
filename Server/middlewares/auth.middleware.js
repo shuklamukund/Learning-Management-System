28979e722cada231166a3e0089063eb07b0bd7de
mukund kumar <mukund5457@gmail.com>
2023-12-22 16:32:18 +0530

    adding various feature in courses routes and also added authorization

--- Server/middlewares/auth.middleware.js
+++ Server/middlewares/auth.middleware.js
@@ -24,6 +24,19 @@ const isLoggedIn=async(req,res,next)=>{
     
 }
 
+// Middleware to check if user is admin or not
+const authorizeRoles = (...roles) =>
+  async (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to view this route", 403)
+      );
+    }
+
+    next();
+}
+
 export {
-  isLoggedIn
+  isLoggedIn,
+  authorizeRoles
 };
--- Server/routes/course.routes.js
+++ Server/routes/course.routes.js
@@ -1,15 +1,23 @@
 import {Router} from 'express'
-import { getAllCourses, getLecturesByCourseId } from '../controllers/course.controller.js';
-import { isLoggedIn } from '../middlewares/auth.middleware.js';
+import { addLectureToCourseById, createCourse, getAllCourses, getLecturesByCourseId, removeCourse, removeLectureFromCourse, updateCourse } from '../controllers/course.controller.js';
+import { authorizeRoles, isLoggedIn } from '../middlewares/auth.middleware.js';
+import upload from '../middlewares/multer.middleware.js';
 
 const router=Router();
 
 router
 .route('/')
-.get(getAllCourses);
+.get(getAllCourses)
+.post(isLoggedIn,authorizeRoles('ADMIN'),upload.single('thumbnail'),createCourse)
+.delete(isLoggedIn, authorizeRoles('ADMIN'), removeLectureFromCourse);
+
 
 router
 .route('/:id')
-.get(isLoggedIn,getLecturesByCourseId);
+.get(isLoggedIn,getLecturesByCourseId)
+.put(isLoggedIn,authorizeRoles('ADMIN'),updateCourse)
+.delete(isLoggedIn,authorizeRoles('ADMIN'),removeCourse)
+.post(isLoggedIn,authorizeRoles('ADMIN'),upload.single('lecture'),addLectureToCourseById)
+
 
 export default router;
