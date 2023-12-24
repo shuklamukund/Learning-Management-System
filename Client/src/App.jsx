188cf7c62d2205ecd31bd7d6cf3ad482e6962811
mukund kumar <mukund5457@gmail.com>
2023-12-25 02:11:59 +0530

    layout and homepage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
-import Footer from './Components/Footer'
+import Homepage from './Pages/HomePage'
+import {Routes,Route} from 'react-router-dom'
 
 function App() {
  
 
   return (
     <>
-     <Footer/>
+    <Routes>
+       <Route path="/" element={<Homepage/>}></Route>
+
+
+
+    </Routes>
     </>
   )
 }
---
+++ Client/src/Layouts/HomeLayout.jsx
@@ -0,0 +1,84 @@
+
+import { FiMenu } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { AiFillCloseCircle } from "react-icons/ai";
+import Footer from "../Components/Footer";
+
+
+function HomeLayout({children}) {
+ 
+  // function to hide the drawer on close button click
+  const hideDrawer = () => {
+    const element = document.getElementsByClassName("drawer-toggle");
+    element[0].checked = false;
+
+    // collapsing the drawer-side width to zero
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = 0;
+  };
+
+  // function for changing the drawer width on menu button click
+  const changeWidth = () => {
+    const drawerSide = document.getElementsByClassName("drawer-side");
+    drawerSide[0].style.width = "auto";
+  };
+
+
+  return (
+    <div className="min-h-[90vh]">
+      {/* adding the daisy ui drawer */}
+      <div className="drawer absolute z-50 left-0 w-fit">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label htmlFor="my-drawer" className="cursor-pointer relative">
+            <FiMenu
+              onClick={changeWidth}
+              size={"32px"}
+              className="font-bold text-white m-4"
+            />
+          </label>
+        </div>
+
+        <div className="drawer-side w-0">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+            {/* close button for drawer */}
+            <li className="w-fit absolute right-2 z-50">
+              <button onClick={hideDrawer}>
+                <AiFillCloseCircle size={24} />
+              </button>
+            </li>
+
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+
+            <li>
+              <Link to={"/courses"}>All Courses</Link>
+            </li>
+
+            <li>
+              <Link to={"/contact"}>Contact Us</Link>
+            </li>
+
+            <li>
+              <Link to={"/about"}>About Us</Link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+
+      {children}
+
+      {/* adding the footer content */}
+      <Footer />
+    </div>
+
+   
+
+ 
+  );
+}
+
+export default HomeLayout;
