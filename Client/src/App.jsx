a73973c1745036d91a4fd69a81ef8fac19fca7c8
mukund kumar <mukund5457@gmail.com>
2023-12-26 12:19:45 +0530

    added about us page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import AboutUs from './Pages/AboutUs'
 import Homepage from './Pages/HomePage'
 import {Routes,Route} from 'react-router-dom'
 
@@ -10,6 +11,7 @@ function App() {
     <>
     <Routes>
        <Route path="/" element={<Homepage/>}></Route>
+       <Route path='/about' element={<AboutUs/>}></Route>
 
 
 
