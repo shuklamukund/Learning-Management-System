4cc424a5e040bc233929f6372798c09301406c01
mukund kumar <mukund5457@gmail.com>
2023-12-26 16:42:57 +0530

    added login flow

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from './Pages/HomePage'
 import {Routes,Route} from 'react-router-dom'
 import Signup from './Pages/Signup'
 import NotFound from './Pages/NotFound'
+import Login from './Pages/Login'
 
 function App() {
  
@@ -15,6 +16,7 @@ function App() {
        <Route path="/" element={<Homepage/>}></Route>
        <Route path='/about' element={<AboutUs/>}></Route>
        <Route path="/signup" element={<Signup/>}></Route>
+       <Route path='/login' element={<Login/>}></Route>
        <Route path='*' element={<NotFound/>}></Route>
 
 
--- Client/src/Redux/Slices/AuthSlice.js
+++ Client/src/Redux/Slices/AuthSlice.js
@@ -28,10 +28,43 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) => {
     }
   });
 
+// function to handle login
+export const login = createAsyncThunk("auth/login", async (data) => {
+    try {
+      let res = axiosInstance.post("/user/login", data);
+  
+      await toast.promise(res, {
+        loading: "Loading...",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to log in",
+      });
+  
+      // getting response resolved here
+      res = await res;
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  });
+
 const authSlice=createSlice({
     name:'auth',
     initialState,
     reducers:{},
+    extraReducers: (builder) => {
+        builder
+          // for user login
+          .addCase(login.fulfilled, (state, action) => {
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role;
+          })
+        }
 });
 
 export const {}=authSlice.actions;
