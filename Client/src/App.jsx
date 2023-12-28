c52d55ace93cfc1d375b3b9c733f6cfe4f17f255
mukund kumar <mukund5457@gmail.com>
2023-12-28 23:24:20 +0530

    added profile page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,6 +12,7 @@ import Denied from './Pages/Denied'
 import CourseDescription from './Pages/Course/CourseDescription'
 import RequireAuth from './Components/Auth/RequireAuth'
 import CreateCourse from './Pages/Course/CreateCourse'
+import Profile from './Pages/User/Profile'
 
 function App() {
  
@@ -29,6 +30,10 @@ function App() {
        <Route path='/course/description' element={<CourseDescription/>}></Route>
        <Route element={<RequireAuth allowedRoles={['ADMIN']}/>}>
        <Route path='/course/create' element={<CreateCourse/>}/>
+       </Route>
+
+       <Route element={<RequireAuth allowedRoles={['ADMIN','USER']}/>}>
+       <Route path='/user/profile' element={<Profile/>}></Route>
        </Route>
        
        <Route path='*' element={<NotFound/>}></Route>
